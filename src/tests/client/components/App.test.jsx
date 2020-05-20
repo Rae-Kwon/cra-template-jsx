00b3f4ff2c17bc8fec12921d5fce765587d2c090
Richard Kwon <rkminjae@gmail.com>
2020-05-20 17:39:27 +1200

    Setup template create-react-app with jsx file extension

---
+++ src/tests/client/components/App.test.jsx
@@ -0,0 +1,9 @@
+// Required imports for React enzyme tests
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../../client/components/App';
+
+// Check test suite works
+test('Test suite is working', () => {
+  expect(1).toBeLessThan(3)
+})
